4268d27b14db278977f43d8b8375803b5f3f22e2
Benjamin Loriot <benjamin.loriot.pro@gmail.com>
2020-03-05 16:16:16 +0100

    ajout de rotation du servo moteur pour tester

--- slave.c
+++ slave.c
@@ -1,11 +1,19 @@
 #include <Wire.h>
+#include <Servo.h>
+
 #define SLAVE_ADDRESS 0x08
 byte data_to_echo = 0;
+
+Servo monServo; 
+
 void setup() 
 {
   Wire.begin(SLAVE_ADDRESS);
   Wire.onReceive(receiveData);
   Wire.onRequest(sendData);
+  monServo.attach(2, 1000, 2000);
+  
+  
 }
 
 
@@ -14,12 +22,14 @@ void loop() {
   delay(1000);              // ne fait rien pendant 1 seconde
   digitalWrite(13, LOW);    // éteint la LED
   delay(1000); 
+  monServo.write(90);       // tourne de 90 DEUX GRAISSE
+
 }
 
 void receiveData(int bytecount) {
-      for (int i = 0; i < bytecount; i++) {
-        data_to_echo = Wire.read();
-      }
+  for (int i = 0; i < bytecount; i++) {
+    data_to_echo = Wire.read();
+  }
 }
 
 
