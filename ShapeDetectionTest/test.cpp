2a5cc23b7b98327da6bb7699ab7f5f573ccc30fd
Benjamin Loriot <benjamin.loriot.pro@gmail.com>
2020-06-21 19:01:05 +0200

    ajout de la detection de cercle et de rectangle , a la base pour une image changement de la source pour la webcam mais ne fonctionne pas vraiment chez moi il faut jouer avec la precision de la fonction approxPolyDP

---
+++ ShapeDetectionTest/test.cpp
@@ -0,0 +1,83 @@
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+#include <iostream>
+using namespace cv;
+using namespace std;
+Mat src_gray;
+int thresh = 100;
+RNG rng(12345);
+void thresh_callback(int, void* );
+int main( int argc, char** argv )
+{
+
+  //https://docs.opencv.org/3.4/da/d0c/tutorial_bounding_rects_circles.html
+
+  // to read a picture remove the loop and VideoCapture
+  //CommandLineParser parser( argc, argv, "{@input | index.png | input image}" );
+  //Mat src = imread( samples::findFile( parser.get<String>( "@input" ) ) );
+
+    Mat src ;
+
+    VideoCapture capture;
+    capture.open(0);
+
+    if(capture.isOpened())
+    {
+        cout << "Capture is opened" << endl;
+        for(;;)
+        {
+            capture >> src;
+            if(src.empty())
+                break;
+
+            cvtColor( src, src_gray, COLOR_BGR2GRAY );
+            blur( src_gray, src_gray, Size(3,3) );
+            const char* source_window = "Source";
+            namedWindow( source_window );
+            imshow( source_window, src );
+
+            const int max_thresh = 255;
+            createTrackbar( "Canny thresh:", source_window, &thresh, max_thresh, thresh_callback );
+            thresh_callback( 0, 0 );
+
+            if(waitKey(10) >= 0)
+                break;
+        }
+    }
+    else
+    {
+        cout << "No capture" << endl;
+
+        waitKey(0);
+    }
+
+
+    return 0;
+}
+void thresh_callback(int, void* )
+{
+    Mat canny_output;
+    Canny( src_gray, canny_output, thresh, thresh*2 );
+    vector<vector<Point> > contours;
+    findContours( canny_output, contours, RETR_TREE, CHAIN_APPROX_SIMPLE );
+    vector<vector<Point> > contours_poly( contours.size() );
+    vector<Rect> boundRect( contours.size() );
+    vector<Point2f>centers( contours.size() );
+    vector<float>radius( contours.size() );
+    for( size_t i = 0; i < contours.size(); i++ )
+    {
+        approxPolyDP( contours[i], contours_poly[i], 3, true );
+        boundRect[i] = boundingRect( contours_poly[i] );
+        minEnclosingCircle( contours_poly[i], centers[i], radius[i] );
+    }
+    Mat drawing = Mat::zeros( canny_output.size(), CV_8UC3 );
+    for( size_t i = 0; i< contours.size(); i++ )
+    {
+        Scalar color = Scalar( rng.uniform(0, 256), rng.uniform(0,256), rng.uniform(0,256) );
+        drawContours( drawing, contours_poly, (int)i, color );
+        rectangle( drawing, boundRect[i].tl(), boundRect[i].br(), color, 2 );
+        circle( drawing, centers[i], (int)radius[i], color, 2 );
+    }
+    imshow( "Contours", drawing );
+}
